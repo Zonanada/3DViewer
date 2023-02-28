b8755c625081724b597e39ef543f57aa82223681
Zona Nada <zonanada@student.21-school.ru>
2023-02-28 14:10:51 +0300

    save

--- src/tests/s21_3DViever_test.c
+++ src/tests/s21_3DViever_test.c
@@ -1,6 +1,6 @@
 #include <check.h>
 
-#include "../code_on_c/parser.h"
+#include "../backend/parser.h"
 
 START_TEST(test_isNum) {
     char symbol1 = 'r';
@@ -12,7 +12,7 @@ START_TEST(test_isNum) {
 
 START_TEST(test_getData) {
     char *filename = calloc(100, sizeof(char));
-    strcpy(filename, "./objFiles/cube.obj" );
+    strcpy(filename, "../objFiles/cube.obj" );
     vertex_index_st *data = getData(filename);
     unsigned arrIndexRes[] = {1, 2, 2, 3, 3, 1, 7, 6, 6, 5, 5, 7, 4, 5, 5, 1, 1, 4, 5, 6, 6, 2, 2, 5, 2, 6, 6, 7, 7, 2, 0, 3, 3, 7, 7, 0, 0, 1, 1, 3, 3, 0, 4, 7, 7, 5, 5, 4, 0, 4, 4, 1, 1, 0, 1, 5, 5, 2, 2, 1, 3, 2, 2, 7, 7, 3, 4, 0, 0, 7, 7, 4};
     double arrVertexRes[] = {0.500000, -0.500000, -0.500000, 0.500000, -0.500000, 0.500000, -0.500000, -0.500000, 0.500000, -0.500000, -0.500000, -0.500000, 0.500000, 0.500000, -0.500000, 0.500000, 0.500000, 0.500000, -0.500000, 0.500000, 0.500000, -0.500000, 0.500000, -0.500000};
@@ -30,7 +30,7 @@ START_TEST(test_getData) {
 
 START_TEST(test_changeState) {
     char *filename = calloc(100, sizeof(char));
-    strcpy(filename, "./objFiles/cube.obj" );
+    strcpy(filename, "../objFiles/cube.obj" );
     double arrVertexRes[] = {-0.150080, 1.310974, -0.121047, 0.622981, 0.953562, 0.724312, 1.234496, 0.329933, -0.098570, 0.461435, 0.687345, -0.943930, -0.834496, 0.350067, 0.098570, -0.061435, -0.007345, 0.943930, 0.550080, -0.630974, 0.121047, -0.222981, -0.273562, -0.724312};
     int result = 0;
     vertex_index_st *data = getData(filename);
